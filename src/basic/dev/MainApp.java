9fd85b708791c982da3fc071782b03d3ad5753ba
LuongMinhDuong <Duong40541@gmail.com>
2021-04-23 19:27:06 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,14 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nameStudent;
+		String idSudent;
+		
+		
+	}
+
+}
